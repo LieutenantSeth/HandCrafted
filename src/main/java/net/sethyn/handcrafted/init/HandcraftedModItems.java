dc09e7f0ac592cdc94711bb230290ad0d37fc40b
steamuser <steamuser@wave-pc>
2026-07-30 17:56:21 +0200

    MCreator Merge commit '13844cd2af8a8d2503e5395b6887cfc8b6740694' into b347131fe29f470ebed6d05741da3228d1bfc29b

--- src/main/java/net/sethyn/handcrafted/init/HandcraftedModItems.java
+++ src/main/java/net/sethyn/handcrafted/init/HandcraftedModItems.java
@@ -3,7 +3,11 @@
  */
 package net.sethyn.handcrafted.init;
 
-import net.sethyn.handcrafted.item.*;
+import net.sethyn.handcrafted.item.CopperWireItem;
+import net.sethyn.handcrafted.item.CopperTubeItem;
+import net.sethyn.handcrafted.item.CopperSpringItem;
+import net.sethyn.handcrafted.item.CopperSheetItem;
+import net.sethyn.handcrafted.item.CopperCogItem;
 import net.sethyn.handcrafted.HandcraftedMod;
 
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -15,12 +19,9 @@ public class HandcraftedModItems {
 	public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(HandcraftedMod.MODID);
 	public static final DeferredItem<Item> COPPER_COG = REGISTRY.register("copper_cog", CopperCogItem::new);
 	public static final DeferredItem<Item> COPPER_WIRE = REGISTRY.register("copper_wire", CopperWireItem::new);
-	public static final DeferredItem<Item> COPPER_MESH = REGISTRY.register("copper_mesh", CopperMeshItem::new);
-	public static final DeferredItem<Item> COPPER_ROD = REGISTRY.register("copper_rod", CopperRodItem::new);
-	public static final DeferredItem<Item> COPPER_SCREW = REGISTRY.register("copper_screw", CopperScrewItem::new);
-	public static final DeferredItem<Item> COPPER_SHEET = REGISTRY.register("copper_sheet", CopperSheetItem::new);
 	public static final DeferredItem<Item> COPPER_TUBE = REGISTRY.register("copper_tube", CopperTubeItem::new);
 	public static final DeferredItem<Item> COPPER_SPRING = REGISTRY.register("copper_spring", CopperSpringItem::new);
+	public static final DeferredItem<Item> COPPER_SHEET = REGISTRY.register("copper_sheet", CopperSheetItem::new);
 	// Start of user code block custom items
 	// End of user code block custom items
 }
